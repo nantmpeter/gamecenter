a2e41c0977b6c67acf2923928e229b13a94c2af5
王佳怡 <wangjiayi.p@gmail.com>
2014-10-29 21:40:51 +0800

    change api params

--- app/Lib/Action/ApiAction.class.php
+++ app/Lib/Action/ApiAction.class.php
@@ -9,7 +9,7 @@ class ApiAction extends Action {
 		$type = $_GET['type'];
 		if($type) {
 			$model = M('charge');
-			$r = $model->where(array('type'=>$type))->select();
+			$r = $model->field(array('price','cmd','spnumber','score'))->where(array('type'=>$type))->select();
 			echo json_encode($r);
 		}else{
 			echo '<h1>404</h1>';
